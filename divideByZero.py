14278b5c9fdb361af5f632cfecf622cfce40d90c
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2025-09-04 08:47:42 -0400

    Update divideByZero.py

--- divideByZero.py
+++ divideByZero.py
@@ -1,5 +1,5 @@
 num1 = input("Enter a Number")
 num2 == 0
-quotient = int(num1)/int(num2)
+quotient = int(num1) / int(num2)
 
-print("{num1} divided by {0} is undefined")
+print(f'{num1} divided by {0} is undefined')
