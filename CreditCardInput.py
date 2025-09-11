0e0a8c30fbd205276b89bc420a151308433895bb
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2025-09-11 09:51:21 -0400

    Credit

---
+++ CreditCardInput.py
@@ -0,0 +1,4 @@
+Card_Number = input("Enter your credit card number")
+Expiration_Date = input("Enter your credit card Expiration Date")
+CCV = input("Enter your CCV")
+print(f'Your credit card information is {Card_Number}, {Expiration_Date}, {CCV}')
