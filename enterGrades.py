1b813152b125e34bef81e3b0ee3990ad2828dc69
SANDHILLS\LH218-23 <jad.talbert@gmail.com>
2025-09-17 10:04:42 -0400

    Comment

--- enterGrades.py
+++ enterGrades.py
@@ -1,7 +1,11 @@
 #This program determines the amount of grades a user has entered.
 done = False
 count = 0
+
+#Ask the user how many grades they will enter
 grades_number = int(input("How many grades will you enter?"))
+
+#Begin Loop
 while count < grades_number:
    count = count + 1
    grade = int(input("Enter a grade"))
