510e49d46a149fa9e8c9b9928b712a31156d2005
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2025-09-18 09:49:29 -0400

    RangeCheck

--- checkRange.py
+++ checkRange.py
@@ -1,10 +1,14 @@
 #This code enables the user to input a number in the range of 0 to 100
-done = False
-count = 0
+Minimum = 0
+Maximum = 100
 Number = int(input("Enter a number"))
-while count < 100:
-    print(Number)
-if Number > 100:
+done = False
+#The loop continues until a number out of range is entered
+while Number < Maximum and Number > Minimum:
+    Number = int(input("Enter a new number"))
+if Number > Maximum:
     print("Sorry the number you entered is out of range!")
-if Number < count:
+if Number < Minimum:
     print("Sorry the number you entered is out of range!")
+    
+    
