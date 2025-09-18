aca2697ea70818d9c011ba7008e77e7ccaef10ad
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2025-09-18 09:20:44 -0400

    Ranges

---
+++ checkRange.py
@@ -0,0 +1,10 @@
+#This code enables the user to input a number in the range of 0 to 100
+done = False
+count = 0
+Number = int(input("Enter a number"))
+while count < 100:
+    print(Number)
+if Number > 100:
+    print("Sorry the number you entered is out of range!")
+if Number < count:
+    print("Sorry the number you entered is out of range!")
