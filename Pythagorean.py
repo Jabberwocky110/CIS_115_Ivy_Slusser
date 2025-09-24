d4c893d11d3abd421e8e3d05b841e6420db2b532
SANDHILLS\LH218-23 <jad.talbert@gmail.com>
2025-09-24 10:08:26 -0400

    Calculating Hypotheneuse

--- Pythagorean.py
+++ Pythagorean.py
@@ -1,6 +1,8 @@
 #This code determines the hypoteneuse of a right triangle
 import math
-def Hypothenuse_calculator():
-   x = int(input("Enter the first side measure."))
-   y = int(input("Enter the second side measure."))
-   c = math.sqrt
+def Hypotheneuse_calculator():
+   x = int(input("Enter the first side measure"))
+   y = int(input("Enter the second side measure"))
+   c = math.sqrt((x * x) + (y * y))
+   print(f'The hypotheneuse measures {c: .2f}.')
+Hypotheneuse_calculator()
