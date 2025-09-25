93613b1467845a9f7d3b560e58ad645945fdf99d
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2025-09-25 14:10:05 -0400

    Reversing

--- ReversingAString.py
+++ ReversingAString.py
@@ -1,1 +1,6 @@
 #This code reverses a string.
+def Reversemystring():
+    word = input("Enter a word or sentence")
+    reversedWord = word[::-1]
+    print(f'Your word or sentence reversed is {reversedWord}')
+Reversemystring()
