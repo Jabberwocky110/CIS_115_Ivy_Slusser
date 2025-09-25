4d4180cf2225d575f1722a40ed1656af22cd609d
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2025-09-25 09:37:00 -0400

    Finish

--- EnergyCalculation.py
+++ EnergyCalculation.py
@@ -7,10 +7,20 @@ Speed_of_Light = float(2.99 * (10 ** 8))
 unit2 = ("m/s")
 unit3 = ("kJ")
 energy2 = int(mass * (Speed_of_Light ** 2))
-
+#This function converts both of the energies to scientific notation.
+def scientific_notation(number):
+   scientific_notation = (f'{number: .2e}')
+   #The scientific notation has to be returned so that it can be printed later in the code.
+   return scientific_notation
 def calculate_energy():
-    m = float(input("Enter the mass of an object in kilograms."))
+    #The user can enter the mass of an object as a float.
+    m = float(input("Enter the mass of your object: The mass should be entered as a numerical value."))
+    #The user entered mass is entered as a numerical value before a unit is added.
+    print(f'The mass of your object is {m} {unit1}')
     energy = int(m * (Speed_of_Light ** 2))
-    print(f'The energy calculated from the mass value you entered is {energy} {unit3}.')
+    #The scientific notation function is called within the print, and the user created energy is printed in scientific notation.
+    print(f'The energy calculated from the mass value you entered is {scientific_notation(energy)} {unit3}.')
+    #The calculate energy function is called.
 calculate_energy()
-print(f'The energy produced from a 3 kg object is {energy2} {unit3}')
+#The assigned mass in printed in scientific notation.
+print(f'The energy produced from a 3 kg object is {scientific_notation(energy2)} {unit3}.')
