7d65aa406e0298ee7af734186037167140287946
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2025-09-25 10:02:52 -0400

    Counter

--- LoopIterations.py
+++ LoopIterations.py
@@ -1,8 +1,9 @@
 #This code determines how many times a loop iterates, using a loop counter.
-def print_iterations():
-    val = int(input("How many times do you want the loop to run?"))
-    loop_Counter = val + 1
-    for val in range(loop_Counter):
-        print(loop_Counter)
-    print(f'The function call looped {loop_Counter} times.')
-print_iterations()
+def print_iterations(val):
+    #The loop within the function will determine how many times the loop is run so val is in the function.
+    for val in range(val +1):
+        print(val)
+#Loop_Counter is added here so the user can enter how many times they want the loop to run.
+loop_Counter = int(input("How many times do you want the loop to run?"))
+print_iterations(loop_Counter)
+print(f'The function call looped {loop_Counter} times.')
