a07848edd15e2416a87086e8eb8be632f3a451ac
SANDHILLS\LH218-23 <jad.talbert@gmail.com>
2025-10-01 10:13:52 -0400

    loop with list

---
+++ for_loop_over_list.py
@@ -0,0 +1,9 @@
+def getMyList():
+    Numbers_By_Ten = [10, 20, 30, 40, 50, 60]
+    iterations = 0
+    for num in Numbers_By_Ten:
+        print(num)
+        iterations = iterations + 1
+    print(f'The number of iterations is {iterations}.')
+getMyList()
+    
