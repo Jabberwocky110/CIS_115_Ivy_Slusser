31ce5195f99060c0a62bba2fe3bc1fdff20577b1
SANDHILLS\LH218-23 <jad.talbert@gmail.com>
2025-10-01 10:26:06 -0400

    nested Loop

---
+++ my_nested_for_loop.py
@@ -0,0 +1,7 @@
+def printWordList():
+    word = ["Apples", "Bananas", "Pears", "Carrots"]
+    for value in word:
+        print(value)
+        for list in value:
+            print(list)
+printWordList()
