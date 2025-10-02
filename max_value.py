5c67bf347541178c56ccf0a3e439a5f36b98706b
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2025-10-02 09:38:45 -0400

    Comments

--- max_value.py
+++ max_value.py
@@ -1,7 +1,9 @@
+#This code determines if a number is larger than another number.
 num1 = int(input("Enter a number"))
 num2 = int(input("Enter a second number"))
 numbers = [num1, num2]
 Larger_Number = max(numbers)
+#The code prints the larger number from the original input.
 if num1 is Larger_Number:
     print(f'The larger number is {num1}.')
 else:
--- my_nested_for_loop.py
+++ my_nested_for_loop.py
@@ -1,4 +1,4 @@
-#This function uses a nested loop and prints strings and characters in a list.
+#This code uses a nested loop and prints strings and characters in a list.
 def printWordList():
     word = ["Apples", "Bananas", "Pears", "Carrots"]
     for value in word:
