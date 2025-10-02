36fba93b163dae3c5f7b9c2e13eca544ed39f8c2
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2025-10-02 09:33:30 -0400

    Month

---
+++ monthOfYear.py
@@ -0,0 +1,4 @@
+def months_of_year():
+   startMonth = input("Enter the month you want to start with")
+   endMonth = input("Enter the monthyou want to end with")
+   months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
