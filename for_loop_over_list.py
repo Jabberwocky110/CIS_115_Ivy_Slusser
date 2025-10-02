dee90e90c4df99a32335871debc206abf7d267d3
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2025-10-02 09:33:53 -0400

    Comment

--- for_loop_over_list.py
+++ for_loop_over_list.py
@@ -1,5 +1,7 @@
+#This function counts the iterations in a loop, using a list.
 def getMyList():
     Numbers_By_Ten = [10, 20, 30, 40, 50, 60]
+    #The counter is started.
     iterations = 0
     for num in Numbers_By_Ten:
         print(num)
