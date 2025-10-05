4f78b7ba0da728a6459b9afc2b8992064e1b16ff
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2025-10-05 18:32:12 -0400

    Comment

--- monthOfYear.py
+++ monthOfYear.py
@@ -1,7 +1,9 @@
+#This code implements a function to print user-defined months of the year.
 def months_of_year():
    months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-   startMonth = int(input("Enter the month you want to start with as a numerical value"))
-   endMonth = int(input("Enter the month you want to end with as a numerical value"))
+   startMonth = int(input("Enter the month you want to start with as a numerical value from 1-12"))
+   endMonth = int(input("Enter the month you want to end with as a numerical value from 1-12, greater than the first month"))
+   #The starting month acts as a counter.
    startMonth = startMonth - 1
    while startMonth < endMonth:
     print(months[startMonth])
