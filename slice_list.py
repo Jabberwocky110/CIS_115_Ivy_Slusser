e8875f1348ded5d5e9252c5df1527058fcc398cd
SANDHILLS\LH218-23 <jad.talbert@gmail.com>
2025-10-06 10:47:15 -0400

    slice

---
+++ slice_list.py
@@ -0,0 +1,3 @@
+months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+def slice_list():
+    months_sliced = months[4:7]
