9ad05825e8a5eb93e55f2eb42937123d59fe724f
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2025-10-09 09:42:56 -0400

    True

--- list_append.py
+++ list_append.py
@@ -7,7 +7,7 @@ def append_to_list():
     list.append(Next_Value)
     #The loop continues until the user decides not to append another value to the list.
     Keep_Going = True
-    while Keep_Going:
+    while Keep_Going is True:
         answer = input("Would you like to enter another value to append to the list? (y or n).")
         if answer == "y":
             Loop_Value = input("Enter your new value")
