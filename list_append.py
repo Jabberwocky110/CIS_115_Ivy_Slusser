442d286232f9efb0a1383e045b4a13498b05fa9d
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2025-10-09 09:07:10 -0400

    List Append

--- list_append.py
+++ list_append.py
@@ -1,8 +1,11 @@
+#This code enables a user to append values to a list.
 Initial_Value = input("Enter a value")
 def append_to_list():
     list = [Initial_Value]
+    #The user is required to append at least two values.
     Next_Value = input("Enter another value")
     list.append(Next_Value)
+    #The loop continues until the user decides not to append another value to the list.
     Keep_Going = True
     while Keep_Going:
         answer = input("Would you like to enter another value to append to the list? (y or n).")
