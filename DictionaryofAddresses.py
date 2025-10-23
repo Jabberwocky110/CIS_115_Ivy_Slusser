5aa59b82a1c03bddd89ec48d58880d2dc05298c5
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2025-10-23 09:42:05 -0400

    State

---
+++ DictionaryofAddresses.py
@@ -0,0 +1,17 @@
+names_and_addresses = [
+    {"firstName": "Anna",
+     "lastName": "Barns",
+     "city": "Montgomery",
+     "state": "Alabama",
+     "zipCode": "12121",
+     "phoneNumber": "121-212-1212"
+     },
+     {"firstName": "Cedric",
+      "lastName": "Davis",
+      "city": "Claymont",
+      "state": "Delaware",
+      "zipCode": "34343",
+      "phoneNumber": "343-434-3434"
+     }
+
+]
