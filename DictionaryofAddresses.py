be8837fe84137a265b5cbf03716ac0319512b69e
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2025-10-25 18:00:10 -0400

    Dictionary

--- DictionaryofAddresses.py
+++ DictionaryofAddresses.py
@@ -2,16 +2,30 @@ names_and_addresses = [
     {"firstName": "Anna",
      "lastName": "Barns",
      "city": "Montgomery",
-     "state": "Alabama",
+     "state": "Alambama",
      "zipCode": "12121",
      "phoneNumber": "121-212-1212"
      },
+
      {"firstName": "Cedric",
       "lastName": "Davis",
       "city": "Claymont",
       "state": "Delaware",
       "zipCode": "34343",
       "phoneNumber": "343-434-3434"
-     }
+     },
 
+    {"firstName": "Eric", 
+     "lastName": "FitzRoy",
+     "city": "Miami",
+     "state": "Florida",
+     "zipCode": "565656",
+     "phoneNumber": "565-656-5656"
+     }
 ]
+def print_dictionary():
+    key = input("Enter what values you want to print")
+    for value in names_and_addresses:
+       item = value[key]
+       print(item)
+print_dictionary()
