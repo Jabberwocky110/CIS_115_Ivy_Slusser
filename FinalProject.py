495b58ed2892738351088e424f1baa882489b580
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2025-10-30 16:50:18 -0400

    Final

--- FinalProject.py
+++ FinalProject.py
@@ -1,3 +1,13 @@
+def billing():
+   print("Enter your billing/shipping information:")
+   First_Name = input("Enter your first name")
+   Last_Name = input("Enter your last name")
+   Address = input("Enter your mailing address")
+   City = input("Enter your city")
+   State = input("Enter your state")
+   Zip_Code = input("Enter you zip or postal code")
+   Email = input("Enter your email address")
+   Phone = input("Enter your phone number")
 print("-" * 49)
 space1 = " " * 17
 space2 = "PRODUCT CATALOG"
@@ -38,3 +48,18 @@ CatologPrint = space5 + line + space6
 CatologPrint2 = line + space6
 for (key1, value1), (key2, value2), CatalogList in zip(CatalogNumbers.items(), Catalog.items(), CatalogList):
    print(f'{key1}{CatologPrint}{key2}{CatalogList}{CatologPrint2}{value2}')
+Product = input("Choose a product ID from the product catalog to continue")
+Quantity = input(f"Enter quantity for product{Product}")
+cart = {
+   
+}
+cart[Product] = Quantity
+answer = input("Would you like to add another product(y or n)?")
+if answer == "y":
+   Product2 = input("Choose a product ID from the product catalog to continue")
+   if Product2 == Product:
+     Quantity = Quantity +1
+if answer == "n":
+   reply = input("Are you ready to check out? (y or n)")
+if reply == "y":
+   billing()
