cbae623916b11cf2954f30869f9e6fad6e505d88
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2025-11-18 09:26:21 -0500

    Speech Recognition

--- PythonSpeech.py
+++ PythonSpeech.py
@@ -1,2 +1,8 @@
 import speech_recognition as sr
 r = sr.Recognizer()
+def speechrecognition():
+    print("What do you want to say?")
+    with sr.Microphone as source:
+      Speech = r.listen(source)
+      Text = r.recognize_google(Speech)
+speechrecognition()
