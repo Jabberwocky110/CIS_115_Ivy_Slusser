2c4fb569ddd04caa3189d40c5810ac75c2941e19
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2025-11-24 10:16:03 -0500

    Save

--- PythonSpeech.py
+++ PythonSpeech.py
@@ -2,5 +2,13 @@ import speech_recognition as sr
 r = sr.Recognizer()
 with sr.Microphone() as source:
   print("What do you want to say?")
+  r = sr.Recognizer()
   Speech = r.listen(source)
-  Text = r.recognize_google(Speech)
+  Text = (Speech)
+  Input = input(Text)
+  RecognizedSpeech = r.recognize_google(Input)
+import os
+os.environ["GEMINI_API_KEY"] = "YOUR_API_KEY"
+from google import genai
+client = genai.Client()
+
