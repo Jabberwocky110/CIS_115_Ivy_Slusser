ede7677ed74839b7c56b212ff09947e738388fff
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2025-11-24 10:36:45 -0500

    TicTacToeArray

---
+++ TicTacToe.py
@@ -0,0 +1,7 @@
+TicTacToeArray = [[0, 0, 0],
+                  [0, 0, 0],
+                  [0, 0, 0]]
+for row in TicTacToeArray:
+    for value in row:
+        print(value, end=" ")
+    print()
